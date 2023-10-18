c5488cad75514cd24aeaed00dcd5b29d0f9a8376
Yolo-cell-hash <keerjar@gmail.com>
2023-10-18 23:58:07 +0530

    Secondary commit

--- app.js
+++ app.js
@@ -1,55 +1,58 @@
 const express = require('express');
-const ejs = require('ejs');
-const bodyParser = require('body-parser');
+const { Client } = require('ssh2');
+const fs = require('fs');
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine","ejs");
+const port = 3000;
+
+app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-var client_ip='';
-var client_uname='';
-var client_pass='';
-
-
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
     res.render('home');
 });
 
-app.post('/',function(req,res){
-    client_uname=req.body.client_uname;
-    client_pass=req.body.client_pass;
-    client_ip= req.body.client_ip;
-    
+app.post('/', (req, res) => {
+    const client_uname = req.body.client_uname;
+    const client_ip = req.body.client_ip;
 
-    if(client_ip.length===0  || client_pass.length===0 || client_uname.length===0){
+    if (client_ip.length === 0 || client_uname.length === 0) {
         console.log("Client Parameters missing!");
         res.redirect('/');
-    }else{
-        console.log("Uname : " + client_uname);
-        console.log("Password : " + client_pass);
-        console.log("Client IP : "+client_ip);
-        res.redirect('/');
+        return;
     }
+
+    console.log("Uname: " + client_uname);
+    console.log("Client IP: " + client_ip);
+
+    // Update this with the actual path to your AWS private key
+    const privateKeyPath = './secret-public-key.pem';
+    
+    const conn = new Client();
+
+    conn.on('ready', () => {
+        console.log('SSH Connection Established');
+        conn.shell((err, stream) => {
+            if (err) throw err;
+            stream.on('close', () => {
+                console.log('SSH Connection Closed');
+                conn.end();
+            }).on('data', (data) => {
+                res.send(`<pre>${data}</pre>`);
+            });
+        });
+    }).on('error', (err) => {
+        console.error('SSH Connection Error: ' + err.message);
+        res.send('SSH Connection Error: ' + err.message);
+    }).connect({
+        host: client_ip,
+        username: client_uname,
+        privateKey: fs.readFileSync(privateKeyPath),
+    });
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-app.listen(port, function(){
-  console.log("Server ativated at port successfully");
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
